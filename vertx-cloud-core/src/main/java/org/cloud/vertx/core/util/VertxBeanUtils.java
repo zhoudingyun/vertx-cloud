10fc7a0a92caa77894cda8ff3155f94caf1839cf
zhoudingyun <dingyun.zhou@narwel.com>
2020-11-03 22:40:56 +0800

    1，fix：修改bean管理类

--- vertx-cloud-core/src/main/java/org/cloud/vertx/core/util/VertxBeanUtils.java
+++ vertx-cloud-core/src/main/java/org/cloud/vertx/core/util/VertxBeanUtils.java
@@ -5,61 +5,33 @@ import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class VertxBeanUtils {
-    private static final Map<String, Object> INSTANCES = new ConcurrentHashMap();
+    private static final Map<Object, Object> INSTANCES = new ConcurrentHashMap();
 
-    public static Object put(String name, Object instance) {
+    public static <K, V> void put(K name, V instance) {
         Objects.requireNonNull(name);
         Objects.requireNonNull(instance);
-        return INSTANCES.put(name, instance);
+        INSTANCES.put(name, instance);
     }
 
-    public static Object putIfAbsent(String name, Object instance) {
+    public static <K, V> void putIfAbsent(K name, V instance) {
         Objects.requireNonNull(name);
         Objects.requireNonNull(instance);
-        return INSTANCES.putIfAbsent(name, instance);
+        INSTANCES.putIfAbsent(name, instance);
     }
 
-    public static Object remove(String name) {
+    public static <K> void remove(K name) {
         Objects.requireNonNull(name);
-        return INSTANCES.remove(name);
+        INSTANCES.remove(name);
     }
 
-    public static <V> V get(String name) {
+    public static <K, V> V get(K name) {
         Objects.requireNonNull(name);
         return (V) INSTANCES.get(name);
     }
 
-    public static <V> V getOrDefault(String name, Object instance) {
+    public static <K, V> V getOrDefault(K name, V instance) {
         Objects.requireNonNull(name);
         Objects.requireNonNull(instance);
         return (V) INSTANCES.getOrDefault(name, instance);
-    }
-
-    public static Object put(Class clazz, Object instance) {
-        Objects.requireNonNull(clazz);
-        Objects.requireNonNull(instance);
-        return put(clazz.getName(), instance);
-    }
-
-    public static Object putIfAbsent(Class clazz, Object instance) {
-        Objects.requireNonNull(clazz);
-        Objects.requireNonNull(instance);
-        return putIfAbsent(clazz.getName(), instance);
-    }
-
-    public static Object remove(Class clazz) {
-        Objects.requireNonNull(clazz);
-        return remove(clazz.getName());
-    }
-
-    public static <V> V get(Class clazz) {
-        Objects.requireNonNull(clazz);
-        return get(clazz.getName());
-    }
-
-    public static <V> V getOrDefault(Class clazz, Object instance) {
-        Objects.requireNonNull(clazz);
-        Objects.requireNonNull(instance);
-        return getOrDefault(clazz.getName(), instance);
     }
 }
