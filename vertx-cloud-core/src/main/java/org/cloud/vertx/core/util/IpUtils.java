93e8ea4aeef29a3e2e98f8fcf3fb039e64bab043
2443548884@qq.com <2443548884@qq.com>
2020-11-19 09:15:28 -0500

    feat:VertxCloudHazelcastCluster.java

---
+++ vertx-cloud-core/src/main/java/org/cloud/vertx/core/util/IpUtils.java
@@ -0,0 +1,31 @@
+package org.cloud.vertx.core.util;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class IpUtils {
+    public static String getLocalHost() {
+        try {
+            Enumeration<NetworkInterface> faces = NetworkInterface.getNetworkInterfaces();
+            while (faces.hasMoreElements()) { // 遍历网络接口
+                NetworkInterface face = faces.nextElement();
+                if (face.isLoopback() || face.isVirtual() || !face.isUp()) {
+                    continue;
+                }
+                Enumeration<InetAddress> address = face.getInetAddresses();
+                while (address.hasMoreElements()) { // 遍历网络地址
+                    InetAddress addr = address.nextElement();
+                    if (!addr.isLoopbackAddress() && addr.isSiteLocalAddress() && !addr.isAnyLocalAddress()) {
+                        return addr.getHostAddress();
+                    }
+                }
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return "0.0.0.0";
+    }
+}
